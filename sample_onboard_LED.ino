53bf20d6b8109c9b282cacedb1c142858e09fc7e
ellablerobotics-dev <ellablerobotics@gmail.com>
2026-09-12 09:51:35 -0700

    Add files via upload

---
+++ sample_onboard_LED.ino
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+#define LED_PIN 2
+
+void setup() {
+    Serial.begin(115200);
+
+    pinMode(LED_PIN, OUTPUT);
+
+    Serial.println("Treasure Hunter Starting!");
+}
+
+void loop() {
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
+}
